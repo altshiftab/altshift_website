da5be3abb03b8ff35514c4f313ffc684ec256c17
vph <vph.persson@gmail.com>
2025-04-06 19:18:40 +0200

    Initial

---
+++ static_content_endpoint_specifications.go
@@ -0,0 +1,3 @@
+package main
+
+//go:generate go run github.com/altshiftab/mux_static_content/cmd/generate_endpoints -path https://github.com/altshiftab/altshift_website/releases/latest/download/dist.zip -variable staticContentEndpointSpecifications -add-path-comment
