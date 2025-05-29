5e324120ba88b1162aa6359af54778927fc44cb8
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-05-29 23:40:35 +0200

    Change contact

--- src/scripts/contact.ts
+++ src/scripts/contact.ts
@@ -7,25 +7,3 @@ import "./custom_elements/altshift_header_www.ts";
 import "./custom_elements/altshift_footer_www.ts";
 
 addErrorEventListeners();
-
-document.addEventListener("DOMContentLoaded", () => {
-    ["mouseover", "click"].forEach(listener_type => {
-        [".email-a", ".phone-a"].forEach(selector => {
-            const href_value = (() => {
-                switch (selector) {
-                    case ".email-a":
-                        return atob("bWFpbHRvOnZpa3Rvci5wZXJzc29uQGFsdHNoaWZ0LnNl");
-                    case ".phone-a":
-                        return atob("dGVsOis0NjczOTI0OTcyNw==");
-                    default:
-                        console.error(`Bad selector: ${selector}`);
-                        return "";
-                }
-            })();
-            document.querySelector(selector)?.addEventListener(listener_type, event => {
-                const anchor = event.currentTarget as HTMLAnchorElement;
-                anchor.href = href_value;
-            }, {once: true});
-        });
-    });
-})
