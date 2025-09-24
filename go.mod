e8416d69121af5c080909f5a6609ccc1968b1396
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2025-09-24 11:51:39 +0200

    Refactor to use library

--- go.mod
+++ go.mod
@@ -1,33 +1,40 @@
 module github.com/altshiftab/altshift_website_service
 
-go 1.24.0
+go 1.24.4
 
-toolchain go1.24.1
+toolchain go1.24.6
 
 require (
-	github.com/Motmedel/utils_go v0.0.205
-	github.com/Motmedel/utils_go/pkg/http/mux v0.0.72
-	github.com/Motmedel/utils_go/pkg/http/problem_detail v1.0.4
-	github.com/altshiftab/gcp_utils/pkg/http v0.0.13
-	github.com/altshiftab/gcp_utils/pkg/log v0.0.9
-	golang.org/x/net v0.40.0
+	github.com/Motmedel/utils_go v0.0.257
+	github.com/Motmedel/utils_go/pkg/http/mux v0.0.96
+	github.com/altshiftab/gcp_utils/pkg/http v0.0.26
+	github.com/altshiftab/gcp_utils/pkg/log v0.0.10
 )
 
 require (
-	github.com/Motmedel/ecs_go v0.0.62 // indirect
-	github.com/Motmedel/gcp_logging_go v0.0.5 // indirect
-	github.com/Motmedel/parsing_utils v0.0.3 // indirect
-	github.com/Motmedel/utils_go/pkg/http/log v0.0.4 // indirect
-	github.com/Motmedel/utils_go/pkg/http/parsing/headers v0.0.22 // indirect
+	github.com/Motmedel/dns_utils v0.0.45 // indirect
+	github.com/Motmedel/dns_utils/pkg/log v0.0.10 // indirect
+	github.com/Motmedel/ecs_go v0.0.64 // indirect
+	github.com/Motmedel/gcp_logging_go v0.0.7 // indirect
+	github.com/Motmedel/parsing_utils v0.0.4 // indirect
+	github.com/Motmedel/utils_go/pkg/http/log v0.0.6 // indirect
+	github.com/Motmedel/utils_go/pkg/http/parsing/headers v0.0.23 // indirect
+	github.com/Motmedel/utils_go/pkg/http/problem_detail v1.0.5 // indirect
 	github.com/Motmedel/utils_go/pkg/net/domain_breakdown v0.0.5 // indirect
-	github.com/altshiftab/gcp_utils v0.0.6 // indirect
+	github.com/altshiftab/gcp_utils v0.0.7 // indirect
 	github.com/altshiftab/mux_static_content/cmd/generate_endpoints v0.0.3 // indirect
-	github.com/gammazero/deque v1.0.0 // indirect
+	github.com/gammazero/deque v1.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/pandatix/go-abnf v0.1.8 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/miekg/dns v1.1.66 // indirect
+	github.com/pandatix/go-abnf v0.3.0 // indirect
 	github.com/vphpersson/code_generation_go v0.0.6 // indirect
-	golang.org/x/sync v0.14.0 // indirect
-	golang.org/x/text v0.25.0 // indirect
+	golang.org/x/mod v0.26.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/tools v0.35.0 // indirect
 )
 
 tool github.com/altshiftab/mux_static_content/cmd/generate_endpoints
--- service.go
+++ service.go
@@ -2,115 +2,28 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	motmedelEnv "github.com/Motmedel/utils_go/pkg/env"
 	motmedelErrors "github.com/Motmedel/utils_go/pkg/errors"
-	motmedelMux "github.com/Motmedel/utils_go/pkg/http/mux"
-	"github.com/Motmedel/utils_go/pkg/http/mux/types/response_error"
-	"github.com/Motmedel/utils_go/pkg/http/problem_detail"
-	motmedelHttpTypes "github.com/Motmedel/utils_go/pkg/http/types"
 	altshiftGcpUtilsHttp "github.com/altshiftab/gcp_utils/pkg/http"
 	altshiftGcpUtilsLog "github.com/altshiftab/gcp_utils/pkg/log"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 	"log/slog"
-	"net/http"
-	"net/url"
-	"time"
 )
-
-func makeMux() (*motmedelMux.Mux, error) {
-	baseUrlString := "https://www.altshift.se"
-	baseUrl, err := url.Parse(baseUrlString)
-	if err != nil {
-		return nil, motmedelErrors.NewWithTrace(fmt.Errorf("url parse: %w", err), baseUrlString)
-	}
-
-	mux := altshiftGcpUtilsHttp.MakeMux(staticContentEndpointSpecifications, nil)
-
-	defaultDocumentHeaders := mux.DefaultDocumentHeaders
-	if defaultDocumentHeaders == nil {
-		return nil, motmedelErrors.NewWithTrace(errors.New("nil default document headers"))
-	}
-
-	contentSecurityPolicy := defaultDocumentHeaders["Content-Security-Policy"]
-	if contentSecurityPolicy != "" {
-		contentSecurityPolicy += "; "
-	}
-	contentSecurityPolicy += "require-trusted-types-for 'script'; trusted-types lit-html"
-	defaultDocumentHeaders["Content-Security-Policy"] = contentSecurityPolicy
-
-	documentEndpointSpecifications := mux.GetDocumentEndpointSpecifications()
-
-	if err := altshiftGcpUtilsHttp.PatchCrawlable(mux, baseUrl, documentEndpointSpecifications); err != nil {
-		return nil, motmedelErrors.New(
-			fmt.Errorf("patch crawlable: %w", err),
-			mux, baseUrl, documentEndpointSpecifications,
-		)
-	}
-
-	if err := altshiftGcpUtilsHttp.PatchErrorReporting(mux, baseUrl); err != nil {
-		return nil, motmedelErrors.New(fmt.Errorf("patch error reporting: %w", err), mux, baseUrl)
-	}
-
-	if err := altshiftGcpUtilsHttp.PatchStrictTransportSecurity(mux); err != nil {
-		return nil, motmedelErrors.New(fmt.Errorf("patch strict transport security: %w", err), mux)
-	}
-
-	altshiftGcpUtilsHttp.PatchSecurityTxt(
-		mux,
-		[]byte(
-			fmt.Sprintf(
-				"Contact: mailto:security@altshift.se\nPreferred-Languages: sv, en\nCanonical: https://www.altshift.se/.well-known/security.txt\nExpires: %s\n",
-				time.Now().UTC().AddDate(1, 0, 0).Format("2006-01-02T15:04:05.000Z"),
-			),
-		),
-	)
-
-	return mux, nil
-}
 
 func main() {
 	logger := altshiftGcpUtilsLog.DefaultFatal(context.Background())
 	slog.SetDefault(logger.Logger)
 
-	port := motmedelEnv.GetEnvWithDefault("PORT", "8080")
-
-	mux, err := makeMux()
+	httpServer, _, err := altshiftGcpUtilsHttp.MakePublicHttpService(
+		"www.altshift.se",
+		motmedelEnv.GetEnvWithDefault("PORT", "8080"),
+		staticContentEndpointSpecifications,
+		[2]string{"altshift.se", "https://www.altshift.se"},
+	)
 	if err != nil {
 		logger.FatalWithExitingMessage("An error occurred when making the mux.", fmt.Errorf("make mux: %w", err))
 	}
-	if mux == nil {
-		logger.FatalWithExitingMessage("Nil mux", nil)
-	}
 
-	mux.ProblemDetailConverter = response_error.ProblemDetailConverterFunction(
-		func(detail *problem_detail.ProblemDetail, negotiation *motmedelHttpTypes.ContentNegotiation) ([]byte, string, error) {
-			data, contentType, err := response_error.ConvertProblemDetail(detail, negotiation)
-			if err != nil {
-				return nil, "", fmt.Errorf("convert problem detail: %w", err)
-			}
-			if contentType == "application/problem+xml" {
-				contentType = "application/xml"
-			}
-			return data, contentType, nil
-		},
-	)
-
-	vhostMux := &motmedelMux.VhostMux{
-		HostToSpecification: map[string]*motmedelMux.VhostMuxSpecification{
-			"www.altshift.se": {Mux: mux},
-			"altshift.se":     {RedirectTo: "https://www.altshift.se"},
-		},
-	}
-	vhostMux.DefaultHeaders = mux.DefaultHeaders
-
-	httpServer := &http.Server{
-		Addr:              fmt.Sprintf(":%s", port),
-		Handler:           h2c.NewHandler(vhostMux, &http2.Server{}),
-		ReadHeaderTimeout: 5 * time.Second,
-	}
 	if err := httpServer.ListenAndServe(); err != nil {
 		logger.FatalWithExitingMessage(
 			"An error occurred when listening and serving.",
